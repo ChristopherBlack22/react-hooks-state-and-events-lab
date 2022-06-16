80d59d84e4b8738e3ccc6833c1818dfe035703ea
Christopher <christopher.black.2209@googlemail.com>
2022-06-16 01:56:38 +0100

    All tests passing, lesson completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false);
+  const liClass = inCart ? "in-cart" : "";
+
+  const handleAddToCart = () => {
+    setInCart(inCart => !inCart)
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart} >Add to Cart</button>
     </li>
   );
 }
